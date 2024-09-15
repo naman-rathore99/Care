b5981296c6dc04609119683fd4e8850188d4958e
naman rathore <namanrathore59@gmail.com>
2024-09-15 05:49:37 +0000

    assets upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex">
-    <h1 className="text-5xl text-bold">hello</h1>
+    <div className="flex max-h-screen h-screen">
+    <section className="remove-scorllbar container my-auto">
+      <div className="sub-container max-w-[496px]">
+
+<Image src="/assets/"
+alt="l"
+className=""
+width={24}
+height={24}/>
+
+      </div>
+
+
+    </section>
+
     </div>
   );
 }
